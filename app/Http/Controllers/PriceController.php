58d7d44cb6cc1b943d7b707b64cbcfa698331303
LukaszST <lukasz.i.urbaniak@gmail.com>
2024-03-15 14:15:03 +0100

    WIP Price show

---
+++ app/Http/Controllers/PriceController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PriceController extends Controller
+{
+    public function showPriceForCard(string $cardId)
+    {
+        //TODO WIP
+    }
+}
