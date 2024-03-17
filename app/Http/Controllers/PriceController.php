bed2c23010a2a027b7bf41cb5c98bcc342502867
LukaszST <lukasz.i.urbaniak@gmail.com>
2024-03-17 01:52:34 +0100

    Work in progress shiowing price for cards

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -9,7 +9,14 @@ class PriceController extends Controller
 {
     public function showPriceForCard(string $cardId)
     {
-        $prices = DB::table('card_market_prices')->where('card_id', '=', $cardId);
+        $prices = DB::table('card_market_prices')->where('card_id', '=', $cardId)->get([
+            'card_id',
+            'average_sell_price',
+            'low_price',
+            'trend_price',
+            'suggested_price',
+            'created_at'
+        ]);
 
         return view('CardPriceChart', [
             'prices' => $prices
