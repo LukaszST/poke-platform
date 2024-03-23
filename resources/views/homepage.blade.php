5fe6b18911f24f487b974c14c033869a307bb687
LukaszST <lukasz.i.urbaniak@gmail.com>
2024-03-23 10:36:20 +0100

    remove welcome view from routing

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,3 +1,21 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
-</div>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    {{--    TODO extend main template--}}
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+
+    <!-- Styles -->
+    {{--    TODO install from npm--}}
+    <script src="https://cdn.tailwindcss.com"></script>
+
+</head>
+<body>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
 
 Route::get('/dashboard', function () {
