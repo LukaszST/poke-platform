b8473219ae41bef5f79be4937ceaf5bca55e9904
LukaszST <lukasz.i.urbaniak@gmail.com>
2024-04-01 11:05:05 +0200

    work in progress wizard for trading cards

--- app/Livewire/OpenTradeWizard.php
+++ app/Livewire/OpenTradeWizard.php
@@ -5,6 +5,7 @@ namespace App\Livewire;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Pokemon\Models\Card;
+use Pokemon\Models\Set;
 use Pokemon\Pokemon;
 
 class OpenTradeWizard extends Component
@@ -12,8 +13,16 @@ class OpenTradeWizard extends Component
     public array $cards;
 
     public ?string $img = null;
+    public ?string $wantedImg = null;
 
     public string $card;
+
+    public array $sets;
+
+    public string $set;
+
+    public ?array $wantedCardsSelect = null;
+    public string $wantedCard;
 
     public function mount()
     {
@@ -25,11 +34,16 @@ class OpenTradeWizard extends Component
         foreach ($cards as $card) {
             /** @var Card $pokemonData */
             $pokemonData = Pokemon::Card()->find($card->card_id);
-            $name = $pokemonData->getName();
-            $number = $pokemonData->getNumber();
-            $set = $pokemonData->getSet()->getName();
-            $cardList[$card->id] = sprintf('%s, number: %s, set: %s', $name, $number, $set);
+            $cardList[$card->id] = $this->prepareCardForSelect($pokemonData);
         }
+
+        $pokemonSets = Pokemon::Set()->all();
+        $sets = [];
+        /** @var Set $pokemonSet */
+        foreach ($pokemonSets as $pokemonSet) {
+            $sets[] = $pokemonSet->getName();
+        }
+        $this->sets = $sets;
 
         $this->cards = $cardList;
 
@@ -47,5 +61,35 @@ class OpenTradeWizard extends Component
         $cardInfo = Pokemon::Card()->find($cards->card_id);
 
         $this->img = $cardInfo->getImages()->getSmall();
+    }
+
+    public function updatedSet($value)
+    {
+        $cardList = Pokemon::Card()->where(['set.name' => $value])->all();
+
+        $cards = [];
+        /** @var Card $card */
+        foreach ($cardList as $card) {
+            $cards[$card->getId()] = $this->prepareCardForSelect($card);
+        }
+
+        $this->wantedCardsSelect = $cards;
+    }
+
+    public function updatedWantedCard($value)
+    {
+        /** @var Card $cardInfo */
+        $cardInfo = Pokemon::Card()->find($value);
+
+        $this->wantedCard = $cardInfo->getImages()->getSmall();
+    }
+
+    private function prepareCardForSelect(Card $card): string
+    {
+        $name = $card->getName();
+        $number = $card->getNumber();
+        $set = $card->getSet()->getName();
+
+        return sprintf('%s, number: %s, set: %s', $name, $number, $set);
     }
 }
