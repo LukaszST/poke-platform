54c7c879122f4a0be6b67bea01751f7040549341
LukaszST <lukasz.i.urbaniak@gmail.com>
2024-04-04 07:37:03 +0200

    New component for loading information

--- resources/views/livewire/open-trade-wizard.blade.php
+++ resources/views/livewire/open-trade-wizard.blade.php
@@ -1,28 +1,7 @@
 <div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
 
-
-    <div wire:loading class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-
-        <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                    <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                        <div class="sm:flex sm:items-start">
-                            {{--                            <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">--}}
-
-                            {{--                            </div>--}}
-                            <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                                <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Please wait...</h3>
-                                <div class="mt-2">
-                                    <p class="text-sm text-gray-500">Your data is loading, please be patient.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div  wire:loading>
+        <livewire:loading-modal></livewire:loading-modal>
     </div>
 
     <form>
