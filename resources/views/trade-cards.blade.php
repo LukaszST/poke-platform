299a952f0922983d2bb07f434b9949a408e9febb
LukaszST <lukasz.i.urbaniak@gmail.com>
2024-04-05 10:18:42 +0200

    Add button for creating trade wizard

--- resources/views/trade-cards.blade.php
+++ resources/views/trade-cards.blade.php
@@ -18,25 +18,76 @@
 <body>
 <livewire:navbar></livewire:navbar>
 
+<div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+    <div class="lg:flex lg:items-center lg:justify-between">
+        <div class="min-w-0 flex-1">
+            <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">Pokemon Trading Card Market</h2>
+        </div>
+        <div class="mt-5 flex lg:ml-4 lg:mt-0">
 
-<ul role="list" class="divide-y divide-gray-100">
+    <span class="sm:ml-3">
 
-    @foreach($openTrades as $openTrade)
-    <li class="flex justify-between gap-x-6 py-5">
-        <div class="flex min-w-0 gap-x-4">
-            <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="{{asset('cards/' . $openTrade->card_id_for_trade . '.png')}}" alt="">
-            <div class="min-w-0 flex-auto">
-                <p class="text-sm font-semibold leading-6 text-gray-900">Trade: {{$openTrade->card_id_for_trade}} for: {{$openTrade->wanted_card_id}}</p>
-                <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{$openTrade->trade_owner}}</p>
+        <a href="/trade/new" wire:navigate>
+            <button type="button" class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <svg class="-ml-0.5 mr-1.5 h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
+                </svg>
+                Add new card trade
+            </button>
+        </a>
+
+    </span>
+
+            <!-- Dropdown -->
+            <div class="relative ml-3 sm:hidden">
+                <button type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400" id="mobile-menu-button" aria-expanded="false" aria-haspopup="true">
+                    More
+                    <svg class="-mr-1 ml-1.5 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                    </svg>
+                </button>
+
+                <!--
+                  Dropdown menu, show/hide based on menu state.
+
+                  Entering: "transition ease-out duration-200"
+                    From: "transform opacity-0 scale-95"
+                    To: "transform opacity-100 scale-100"
+                  Leaving: "transition ease-in duration-75"
+                    From: "transform opacity-100 scale-100"
+                    To: "transform opacity-0 scale-95"
+                -->
+                <div class="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="mobile-menu-button" tabindex="-1">
+                    <!-- Active: "bg-gray-100", Not Active: "" -->
+                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="mobile-menu-item-0">Edit</a>
+                    <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="mobile-menu-item-1">View</a>
+                </div>
             </div>
         </div>
-        <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <p class="text-sm leading-6 text-gray-900">{{$openTrade->short_note}}</p>
-{{--            <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>--}}
-        </div>
-    </li>
-    @endforeach
-</ul>
+    </div>
+
+
+    <ul role="list" class="divide-y divide-gray-100">
+
+        @foreach($openTrades as $openTrade)
+            <li class="flex justify-between gap-x-6 py-5">
+                <div class="flex min-w-0 gap-x-4">
+                    <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="{{asset('cards/' . $openTrade->card_id_for_trade . '.png')}}" alt="">
+                    <div class="min-w-0 flex-auto">
+                        <p class="text-sm font-semibold leading-6 text-gray-900">Trade: {{$openTrade->card_id_for_trade}} for: {{$openTrade->wanted_card_id}}</p>
+                        <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{$openTrade->trade_owner}}</p>
+                    </div>
+                </div>
+                <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                    <p class="text-sm leading-6 text-gray-900">{{$openTrade->short_note}}</p>
+                    {{--            <p class="mt-1 text-xs leading-5 text-gray-500">Last seen <time datetime="2023-01-23T13:23Z">3h ago</time></p>--}}
+                </div>
+            </li>
+        @endforeach
+    </ul>
+
+</div>
+
 
 
 </body>
