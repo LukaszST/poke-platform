c96c43aeb14d64214758236e8a814a6da9f3ed7e
LukaszST <lukasz.i.urbaniak@gmail.com>
2024-04-08 21:09:24 +0200

    Bid wizard wip

--- resources/views/BidTrade.blade.php
+++ resources/views/BidTrade.blade.php
@@ -1,4 +1,25 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-    WIP!!! Work In Progress
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    {{--    TODO extend main template--}}
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <!-- Styles -->
+    {{--    TODO install from npm--}}
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<livewire:navbar></livewire:navbar>
+
+<div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+
 </div>
+</body>
+</html>
